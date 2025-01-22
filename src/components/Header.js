1f7e18b032df832a929c15bd73f4735cd166be66
ddoris2106 <daundre.doris16@myhunter.cuny.edu>
2025-01-21 23:35:12 -0500

    Created header for app

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+// When game is over, add correct word to top of header
+
+function Header() {
+	return (
+		<div>
+			<h1
+				id="title"
+				className="text-appGrey text-lg text-center"
+			>
+				Word500
+			</h1>
+		</div>
+	);
+}
+
+export default Header;
