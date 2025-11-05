0e68fd59d2fc0cff7b8323f9f1bae96fdc577c0a
ddoris2106 <daundre.doris16@myhunter.cuny.edu>
2025-11-05 00:53:35 -0500

    formatting

--- src/state/recoil_state.js
+++ src/state/recoil_state.js
@@ -1,1 +1,25 @@
 import { atom, selector } from "recoil";
+
+const currentRow = atom({
+	key: "CurrentRowIndex",
+	default: 0,
+});
+
+const currentTileIndex = atom({
+	key: "CurrentTileIndex",
+	default: 0,
+});
+
+// Store the current user submitted guess
+const currentGuess = aton({
+	key: "UserGuess",
+	default: "",
+});
+
+// Record the guesses the user submitted
+const guessHistory = atom({
+	key: "UserGuessHistory",
+	default: [],
+});
+
+export { currentRow, currentTileIndex, currentGuess, guessHistory };
