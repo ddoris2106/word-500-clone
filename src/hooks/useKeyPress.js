494b1c9f87eb43b35700865b938c2767f1e7f6d1
ddoris2106 <daundre.doris16@myhunter.cuny.edu>
2025-12-15 00:35:24 -0500

    Began adding key press functionality in psuedocode

--- src/hooks/useKeyPress.js
+++ src/hooks/useKeyPress.js
@@ -1,19 +1,34 @@
 import { useEffect } from "react";
 
-/**
- * useKeyPress("Escape", handler) 
- * useKeyPress(["Escape", "Enter"], handler)
- */
+/*
+Add handlers to modify current Guess State
+ - also updates currentTileIndex state
+Add handler to add to guessHistory state 
+ - also updates current Row  
+*/
+
+// Check if guess is5 letters long
+// if less, add to current guess
+// If 5, allow enter button
+
 export function useKeyPress(targetKeys, handler, deps = []) {
-  useEffect(() => {
-    const keys = Array.isArray(targetKeys) ? targetKeys : [targetKeys];
+	useEffect(() => {
+		// Keys to listen for (all letters, backspace, underscore, and enter keys )
+		// On enter, submit guess to recoil state
+		const keys = Array.isArray(targetKeys) ? targetKeys : [targetKeys];
 
-    function onKeyDown(e) {
-      if (keys.includes(e.key)) handler(e);
-    }
+		// Check if key pressed is a letter key, add to currentGuess state
+		// If key pressed is a backspace, remove last letter from state
+		// Check if key pressed is an underscore
+		// --do not submit if enter is pressed
+		// If enter is pressed: update guessHistory state by adding currentGuess state
 
-    window.addEventListener("keydown", onKeyDown);
-    return () => window.removeEventListener("keydown", onKeyDown);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, deps); // caller controls deps (e.g., [handler])
+		// --check if guess is in word list; if not, change color to red and strike through
+		function onKeyUp(e) {
+			if (keys.includes(e.key)) handler(e);
+		}
+
+		window.addEventListener("keyup", onKeyUp);
+		return () => window.removeEventListener("keyup", onKeyUp);
+	}, deps);
 }
