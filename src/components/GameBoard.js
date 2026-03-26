dbe797afd86d3510660eec47ce5885bbce7ac6b4
ddoris2106 <daundre.doris16@myhunter.cuny.edu>
2026-03-25 23:52:12 -0400

    Updatad GameBoard component to call key press and passed valid letter args

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Grid from "./Grid";
 import { renderGrid } from "../hooks/renderGrid";
+import { useKeyPress } from "../hooks/useKeyPress";
+import { useStateAccess } from "../utils/state_access";
 
 // Setup Game Board
 // 8x5 grid next to a 8x3 grid
@@ -28,6 +30,36 @@ function GameBoard() {
 
 	const guessGrid = renderGrid(gridData.guessGrid, bgColors);
 	const resultsGrid = renderGrid(gridData.resultsGrid, bgColors);
+
+	useKeyPress([
+		"A",
+		"B",
+		"C",
+		"D",
+		"E",
+		"F",
+		"G",
+		"H",
+		"I",
+		"J",
+		"K",
+		"L",
+		"M",
+		"N",
+		"O",
+		"P",
+		"Q",
+		"R",
+		"S",
+		"T",
+		"U",
+		"V",
+		"W",
+		"X",
+		"Y",
+		"Z",
+		"_",
+	]);
 
 	return (
 		<div className="flex flex-row content-center justify-center w-full gap-x-4">
